1c0ceb4249f36d9c81eedcd610e0ecf4877ced97
cjax44 <cjax44@gmail.com>
2019-05-22 11:35:17 -0400

    Added entities and repos and some controllers

---
+++ src/main/java/com/prs/business/PurchaseRequest.java
@@ -0,0 +1,130 @@
+package com.prs.business;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Entity
+public class PurchaseRequest {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name = "userID")
+	private User user;
+	private String description;
+	private String justification;
+	private Date dateNeeded;
+	private String deliveryMode;
+	private String status;
+	private double total;
+	private Date submittedDate;
+	private String reasonForRejection;
+	
+	
+	
+	
+	public PurchaseRequest() {
+		
+	}
+
+	public PurchaseRequest(int id, User user, String description, String justification,
+			Date dateNeeded, String deliveryMode, String status, double total, Date submittedDate,
+			String reasonForRejection) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.description = description;
+		this.justification = justification;
+		this.dateNeeded = dateNeeded;
+		this.deliveryMode = deliveryMode;
+		this.status = status;
+		this.total = total;
+		this.submittedDate = submittedDate;
+		this.reasonForRejection = reasonForRejection;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUserID(User userID) {
+		this.user = userID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getJustification() {
+		return justification;
+	}
+
+	public void setJustification(String justification) {
+		this.justification = justification;
+	}
+
+	public Date getDateNeeded() {
+		return dateNeeded;
+	}
+
+	public void setDateNeeded(Date dateNeeded) {
+		this.dateNeeded = dateNeeded;
+	}
+
+	public String getDeliveryMode() {
+		return deliveryMode;
+	}
+
+	public void setDeliveryMode(String deliveryMode) {
+		this.deliveryMode = deliveryMode;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public Date getSubmittedDate() {
+		return submittedDate;
+	}
+
+	public void setSubmittedDate(Date submittedDate) {
+		this.submittedDate = submittedDate;
+	}
+
+	public String getReasonForRejection() {
+		return reasonForRejection;
+	}
+
+	public void setReasonForRejection(String reasonForRejection) {
+		this.reasonForRejection = reasonForRejection;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/prs/web/VendorController.java
@@ -0,0 +1,117 @@
+	package com.prs.web;
+
+	import java.util.Optional;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.web.bind.annotation.DeleteMapping;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.PathVariable;
+	import org.springframework.web.bind.annotation.PostMapping;
+	import org.springframework.web.bind.annotation.PutMapping;
+	import org.springframework.web.bind.annotation.RequestBody;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RequestParam;
+	import org.springframework.web.bind.annotation.RestController;
+
+	import com.prs.business.JsonResponse;
+	import com.prs.business.User;
+import com.prs.business.Vendor;
+import com.prs.db.VendorRepository;
+
+@RestController
+@RequestMapping("/vendors")
+public class VendorController {
+	
+
+	
+		
+		@Autowired
+		private VendorRepository vendorRepo;
+
+		@GetMapping("/")
+		public JsonResponse getAll() {
+			JsonResponse jr = null;
+			try {
+				jr = JsonResponse.getInstance(vendorRepo.findAll());
+
+			} catch (Exception e) {
+				jr = JsonResponse.getInstance(e);
+			}
+			return jr;
+
+		}
+
+		@GetMapping("/{id}")
+		public JsonResponse getAll(@PathVariable int id) {
+			JsonResponse jr = null;
+			try {
+				Optional<Vendor> p = vendorRepo.findById(id);
+				if (p.isPresent())
+					jr = JsonResponse.getInstance(p);
+				else
+					jr = JsonResponse.getInstance("No User found for id: " + id);
+
+			} catch (Exception e) {
+				jr = JsonResponse.getInstance(e);
+			}
+			return jr;
+
+		}
+		
+		@DeleteMapping("/")
+		public JsonResponse delete(@RequestBody Vendor p) {
+			JsonResponse jr = null;
+			// NOTE: May want to enhance exception handling
+			try {
+				if (vendorRepo.existsById(p.getId())) {
+					vendorRepo.delete(p);
+				jr = JsonResponse.getInstance("User deleted.");
+				}
+				else {
+					jr = JsonResponse.getInstance("User ID: " + p.getId() + " does not exist and you are attempting to delete it");
+				}
+
+			} catch (Exception e) {
+				jr = JsonResponse.getInstance(e);
+			}
+			return jr;
+			
+		}
+		
+		@PutMapping("/")
+		public JsonResponse update(@RequestBody Vendor p) {
+			JsonResponse jr = null;
+			// NOTE: May want to enhance exception handling
+			try {
+				if (vendorRepo.existsById(p.getId())) {
+				jr = JsonResponse.getInstance(vendorRepo.save(p));
+				}
+				else {
+					jr = JsonResponse.getInstance("User ID: " + p.getId() + " does not exist and you are attempting to save it");
+				}
+
+			} catch (Exception e) {
+				jr = JsonResponse.getInstance(e);
+			}
+			return jr;
+			
+		}
+
+		
+		@PostMapping("/")
+		public JsonResponse add(@RequestBody Vendor p) {
+			JsonResponse jr = null;
+			// NOTE: May want to enhance exception handling
+			try {
+				jr = JsonResponse.getInstance(vendorRepo.save(p));
+
+			} catch (Exception e) {
+				jr = JsonResponse.getInstance(e);
+			}
+			return jr;
+
+		}
+		
+
+	}
+
